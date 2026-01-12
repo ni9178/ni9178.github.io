529a3b830fe08c5db37c61bec75014b81d7f62e9
ni9178 <17513411682@163.com>
2026-01-12 16:36:17 +0800

    Site updated: 2026-01-12 16:36:16

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/3eeb.html","posts/13178.html","posts/123c.html","posts/10219.html","posts/7737.html","posts/64332.html","posts/38054.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
